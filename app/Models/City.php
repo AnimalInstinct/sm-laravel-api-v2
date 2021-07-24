9706db3351fc9e2cfe57c2081dd2b042f6bca4fb
Alexander <a.makarov@molinos.ru>
2021-07-24 12:56:21 +0700

    models copies, spatie laravel-permission installed

---
+++ app/Models/City.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Modelsphp;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+
+class City extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = ['name'];
+
+    public function districts()
+    {
+        return $this->hasMany('App\District');
+    }
+
+    public function country()
+    {
+        return $this->belonsTo('App\Country');
+    }
+
+    public function places()
+    {
+        return $this->hasMany('App\Place');
+    }
+}
